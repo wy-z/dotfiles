32d043599dfb3e93981420150ac2abfe04cd6988
weiyang <weiyang.ones@gmail.com>
2021-03-14 00:49:15 +0800

    [fish] Refine prompt
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,15 @@ alias g=git
 # direnv
 direnv hook fish | source
 
+# prompt
+functions -c fish_prompt _old_fish_prompt
+function fish_prompt
+    if set -q VIRTUAL_ENV # python virtualenv
+        echo -n -s "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+    end
+    _old_fish_prompt
+end
+
 # source fishrc
 set alias_file ~/.fishrc
 if test -e {$alias_file}
