50668617bd978d9adb40de341797530e02ee7853
weiyang <weiyang.ones@gmail.com>
2021-03-13 23:25:43 +0800

    Replace zsh with fish
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+# alias
+alias g=git
+
+# autojump
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+
+# direnv
+direnv hook fish | source
+
+# source fishrc
+set alias_file ~/.fishrc
+if test -e {$alias_file}
+    source {$alias_file}
+end
